bf47b0d0d403bba2bf559b0ed4dccafd511c2186
ZENSE <125728307+ZENSE-TH@users.noreply.github.com>
2024-11-07 18:51:02 +0700

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ module.exports = {
   SecondButtonName: "Song of the Day.",
   SecondButtonUrl: "https://youtu.be/ZPyxsR9CqUw?si=bIBjO2p2U85uqLEj",
   LargeImage: "https://media.discordapp.net/attachments/1226028570556498071/1304047546787696661/2970.gif?ex=672df86b&is=672ca6eb&hm=8d0459b72e58eeeee6cd9bc9c5726afd5effa876032ebd127770539211c0ef26&=&width=400&height=300", // DISCORD CDN IMAGE ONLY
-  LargeText: "กำลังตามหาแมวแปลกหน้าบางตัว", // hover text for large image
+  LargeText: "หากไม่ใช่ดอกทานตะวัน ฉันจะเป็นแคคตัสในโรงเรือน", // hover text for large image
   SmallImage: "https://cdn.discordapp.com/attachments/1226028570556498071/1304047500474187776/chi-cat.gif?ex=672df860&is=672ca6e0&hm=c03ebba584e364fc27eb4ceaf0e708d3afdb6c2fbb82f5c55dbbe40b563d3f6f&", // DISCORD CDN IMAGE ONLY
   SmallText: "เมี๊ยวๆ", // hover text for small image
 };
