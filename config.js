ff74c7203c84c86c3d29f3e5cfaf2547795c6920
ZENSE <125728307+ZENSE-TH@users.noreply.github.com>
2024-11-07 18:57:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,12 +6,12 @@ module.exports = {
   Name: "TWICH STREAMER",
   State: "24/7hr.",
   Details: "ᴡɪɴᴛᴇʀ ɪs ᴄᴏᴍɪɴɢ",
-  FirstButtonName: "Follow Mw",
+  FirstButtonName: "Follow Me",
   FirstButtonUrl: "https://www.instagram.com/_iswinter",
   SecondButtonName: "Song of the Day.",
   SecondButtonUrl: "https://youtu.be/ZPyxsR9CqUw?si=bIBjO2p2U85uqLEj",
   LargeImage: "https://media.discordapp.net/attachments/1226028570556498071/1304047546787696661/2970.gif?ex=672df86b&is=672ca6eb&hm=8d0459b72e58eeeee6cd9bc9c5726afd5effa876032ebd127770539211c0ef26&=&width=400&height=300", // DISCORD CDN IMAGE ONLY
   LargeText: "หากไม่ใช่ดอกทานตะวัน ฉันจะเป็นแคคตัสในโรงเรือน", // hover text for large image
   SmallImage: "https://cdn.discordapp.com/attachments/1226028570556498071/1304047500474187776/chi-cat.gif?ex=672df860&is=672ca6e0&hm=c03ebba584e364fc27eb4ceaf0e708d3afdb6c2fbb82f5c55dbbe40b563d3f6f&", // DISCORD CDN IMAGE ONLY
-  SmallText: "เมี๊ยวๆ", // hover text for small image
+  SmallText: "❄️❄️❄️", // hover text for small image
 };
